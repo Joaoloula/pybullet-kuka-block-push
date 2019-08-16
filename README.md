b51adaaa2924a7b77128080078942acf93af0e08
João <jloula@mit.edu>
2019-08-16 16:29:22 -0400

    added simulation timings

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pybullet-kuka-block-push
+
+Note: need to figure out how to speed up simulations without degrading contact
+forces. Currently at about 3s for running a simulation with 30k timesteps and
+delta_y = 5e-6 and the default timestep of 240Hz, using pybullet.DIRECT. Fiddling
+with these any of variables seems to result in spiking behavior of the contact forces again.
+Currently, 42% of the runtime of _main_ is due to p.stepSimulation().
--- kukaBlock_separate_forces.py
+++ kukaBlock_separate_forces.py
@@ -1,42 +1,40 @@
-import pybullet as p
+
+import pybullet_utils.bullet_client as bc
 import pybullet_data
-import time
+import pybullet
 import math
-from datetime import datetime
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-import pdb
+import time
 
 
 def main():
-  p.connect(p.GUI)
+  start = time.time()
+  p = bc.BulletClient(connection_mode=pybullet.DIRECT)
+  p.setTimeStep(1/240.)
   p.setAdditionalSearchPath(pybullet_data.getDataPath())
   p.loadURDF("plane.urdf", [0, 0, -0.05], useFixedBase=True)
   kukaId = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0], useFixedBase=True)
   p.resetBasePositionAndOrientation(kukaId, [0, 0, 0], [0, 0, 0, 1])
   kukaEndEffectorIndex = 6
   numJoints = p.getNumJoints(kukaId)
-  if (numJoints != 7):
-    exit()
+  blockId = p.loadURDF("urdfs/block_big.urdf", [-0.4, 0, .1], useFixedBase=0)
 
-  blockId = p.loadURDF("block_big.urdf", [-0.4, 0, .1])
-
-  #lower limits for null space
-  ll = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
-  #upper limits for null space
-  ul = [.967, 2, 2.96, 2.29, 2.96, 2.09, 3.05]
-  #joint ranges for null space
-  jr = [5.8, 4, 5.8, 4, 5.8, 4, 6]
   #restposes for null space
   rp = [0, 0, 0, 0.5 * math.pi, 0, -math.pi * 0.5 * 0.66, 0]
   #joint damping coefficents
-  jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+  damping = 1e-1
+  jd = [damping] * 7
 
   for i in range(numJoints):
     p.resetJointState(kukaId, i, rp[i])
 
+  p.setPhysicsEngineParameter(constraintSolverType=p.CONSTRAINT_SOLVER_LCP_SI)
+
   p.setGravity(0, 0, -10)
   t = 0.
+  speedup = 1
   prevPose = [0, 0, 0]
   prevPose1 = [0, 0, 0]
   hasPrevPose = 0
@@ -45,19 +43,12 @@ def main():
   count = 0
   useOrientation = 1
   useSimulation = 1
-  useRealTimeSimulation = 1
-  useLogger = False
-  p.setRealTimeSimulation(useRealTimeSimulation)
 
   #trailDuration is duration (in seconds) after debug lines will be removed automatically
   #use 0 for no-removal
   trailDuration = 15
 
-  if useLogger:
-    logId1 = p.startStateLogging(p.STATE_LOGGING_GENERIC_ROBOT, "LOG0001.txt", [0, 1, 2])
-    logId2 = p.startStateLogging(p.STATE_LOGGING_CONTACT_POINTS, "LOG0002.txt", bodyUniqueIdA=2)
-
-  simLength = 6000
+  simLength = 30000
   x = -0.4
   y = -0.2
   z = 0.01
@@ -65,18 +56,18 @@ def main():
   orn = p.getQuaternionFromEuler([0, -math.pi, 0])
 
   traj = np.zeros((simLength, 3))
-  contactForceRobotBlock0 = np.zeros((simLength, ))
-  contactForceRobotBlock1 = np.zeros((simLength, ))
+  contactForceRobotBlock = np.zeros(simLength)
   jointInds = [i for i in range(numJoints)]
-  posGains = [0.3] * numJoints
-  velGains = [1.0] * numJoints
+  penetrations = np.zeros(simLength)
+
+  p.changeDynamics(blockId, linkIndex=-1, mass=0.7)
 
   for simTime in range(simLength):
     p.stepSimulation()
 
     # set end effector pose
     dx = 0.00
-    dy = 0.00005
+    dy = 5e-6 # 5e-5
     dz = 0.00
     x += dx
     y += dy
@@ -90,44 +81,35 @@ def main():
                                               orn,
                                               jointDamping=jd)
 
-    for i in range(numJoints):
-      p.setJointMotorControl2(bodyIndex=kukaId,
-                              jointIndex=i,
+
+
+    p.setJointMotorControlArray(bodyIndex=kukaId,
+                              jointIndices=range(numJoints),
                               controlMode=p.POSITION_CONTROL,
-                              targetPosition=jointPoses[i],
-                              targetVelocity=0,
-                              force=500,
-                              positionGain=0.3,
-                              velocityGain=1)
+                              targetPositions=jointPoses[:numJoints],
+                              forces=[100]*numJoints,
+                              positionGains=[0.3]*numJoints,
+                              velocityGains=[1]*numJoints)
 
     # get information on contact between the block and the robot
     contactInfo = p.getContactPoints(kukaId, blockId)
 
     if len(contactInfo) > 0:
+      contactForceRobotBlock[simTime] = sum([c[9] for c in contactInfo])
+      penetrations[simTime] = max([c[8] for c in contactInfo])
 
-      contactForceRobotBlock0[simTime] = contactInfo[0][9]
-
-      if len(contactInfo) > 1:
-          contactForceRobotBlock1[simTime] = contactInfo[1][9]
-
-      if len(contactInfo) > 2:
-          print("too many robot/block contacts!")
-          pdb.set_trace()
-
-
-  plt.plot(contactForceRobotBlock0)
-  plt.title("first contact force between robot and block")
-  plt.savefig("first_force.png")
-  plt.show()
-  plt.plot(contactForceRobotBlock1)
-  plt.title("second contact force between robot and block")
-  plt.savefig("second_force.png")
-  plt.show()
-  plt.plot(contactForceRobotBlock0 + contactForceRobotBlock1)
-  plt.title("sum of the two contact forces between robot and block")
+  end = time.time()
+  print("time elapsed: ", end - start)
+  plt.plot(contactForceRobotBlock)
+  plt.title("total contact force between robot and block")
   plt.savefig("sum_forces.png")
   plt.show()
 
+  results = {
+    "contact force": contactForceRobotBlock,
+    "parameters": p.getDynamicsInfo(blockId, -1)}
 
-if __name__=='__main__':
+  # pickle.dump(results, open("results.pickle", "wb"))
+
+if __name__ == '__main__':
   main()
